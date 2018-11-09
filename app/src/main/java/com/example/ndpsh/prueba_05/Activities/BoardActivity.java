c22980e5836e728e7f2354c58ced69863f2eb55e
Steinspass <37332713+Steinspass@users.noreply.github.com>
2018-11-09 16:25:26 +0100

    final changed

--- app/src/main/java/com/example/ndpsh/prueba_05/Activities/BoardActivity.java
+++ app/src/main/java/com/example/ndpsh/prueba_05/Activities/BoardActivity.java
@@ -6,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -63,9 +66,7 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
             }
         });
 
-        realm.beginTransaction();
-        realm.deleteAll();
-        realm.commitTransaction();
+        registerForContextMenu(listView);
     }
         //** CRUD Actions **/
         private void createdNewBoard(String boardName){
@@ -83,6 +84,27 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
             //                  }
             // });
 
+        }
+
+
+        private void deleteBoard(Board board) {
+            realm.beginTransaction();
+            board.deleteFromRealm();
+            realm.commitTransaction();
+
+        }
+
+        private void EditBoard(String newName, Board board){
+            realm.beginTransaction();
+            board.setTitle(newName);
+            realm.copyToRealmOrUpdate(board);
+            realm.commitTransaction();
+        }
+
+        private void deleteAllBoard() {
+            realm.beginTransaction();
+            realm.deleteAll();
+            realm.commitTransaction();
         }
 
 
@@ -115,6 +137,87 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         dialog.show();
 
     }
+
+    private void showAlertForEditingBoard(String title, String message, final Board board){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if(title != null)builder.setTitle(title);
+        if(message != null)builder.setMessage(message);
+
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
+        builder.setView(viewInflated);
+
+
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewBoard);
+        input.setText(board.getTitle());
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String boardName = input.getText().toString().trim();
+                if (boardName.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "The name is required to edit the current board", Toast.LENGTH_LONG).show();
+                } else if (boardName.equals(board.getTitle())) {
+                    Toast.makeText(getApplicationContext(), "The name is the same than it was before", Toast.LENGTH_LONG).show();
+                }else {
+                    EditBoard(boardName, board);
+                }
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    /* Events */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_board, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.delete_All:
+                deleteAllBoard();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle(boards.get(info.position).getTitle());
+        getMenuInflater().inflate(R.menu.context_menu_board,menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete_Board:
+                deleteBoard(boards.get(info.position));
+
+                return true;
+            case R.id.edit_board:
+                showAlertForEditingBoard("Edit Board", "Change the name of the board", boards.get(info.position));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 
 
     @Override
